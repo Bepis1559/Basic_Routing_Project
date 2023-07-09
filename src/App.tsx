a5a91f4f3e6f25512b3f67ccc97518939b8456da
Bepis1559 <axl2001@abv.bg>
2023-07-09 16:19:31 +0300

    adding ScrollRestoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { ReactElement } from "react";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, ScrollRestoration, useNavigation } from "react-router-dom";
 
 export function App(): ReactElement {
   const { state } = useNavigation();
 
   return (
     <>
+      <ScrollRestoration />
       <Navbar />
       {state == "loading" ? (
         <>
